f6640414991b7cf9d8ef7c2bc89a922f3f7df99e
wanghao <hao.wang@hacknocraft.com>
2017-05-23 23:52:37 -0400

    first commit

---
+++ HCSocialSignIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HCSocialSignIn
+//
+//  Created by HAO WANG on 5/23/17.
+//  Copyright © 2017 Hacknocraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
